2c96d94c140b015c503972d4f04aa6f7a00062b1
CircuitRCAY <circuit@circuitrcay.me>
2018-03-09 12:15:14 +1100

    Problem 2 finished

---
+++ src/main/kotlin/me/circuitrcay/euler/challenges/Problem2.kt
@@ -0,0 +1,23 @@
+package me.circuitrcay.euler.challenges
+
+import me.circuitrcay.euler.Problem
+
+class Problem2 : Problem<String>() {
+    override fun calculate(): Any {
+        var sum = 0
+
+        var num1 = 1; var num2 = 2
+
+        while (num2 < 4000000) {
+            if(num2 % 2 == 0) sum += num2
+
+            val temp = num2
+
+            num2 = num1 + num2
+            num1 = temp
+
+        }
+
+        return sum
+    }
+}
