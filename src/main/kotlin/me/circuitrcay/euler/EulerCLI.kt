74153b316f7f54d20ac1f42bc5aa4210db42f16f
Adam Ratzman <adam@adamratzman.com>
2018-03-08 20:39:34 -0500

    problem 4

--- src/main/kotlin/me/circuitrcay/euler/EulerCLI.kt
+++ src/main/kotlin/me/circuitrcay/euler/EulerCLI.kt
@@ -4,14 +4,19 @@ import java.util.*
 
 fun main(args: Array<String>) {
     println("This is Project Euler.")
-    val scanner = Scanner(System.`in`)
-    val command = scanner.nextLine()
+    while (true) {
+        val scanner = Scanner(System.`in`)
+        val command = scanner.nextLine()
 
-    val problemArray = ProblemLoader.getClasses("me.circuitrcay.euler")
-    problemArray.forEach { problem ->
-       if(problem.name.contains(command.toRegex())) {
-         val challenge = problem.newInstance() as Problem<*>
-         challenge.run()
-       }
+        val problemArray = ProblemLoader.getClasses("me.circuitrcay.euler")
+        var found = false
+        problemArray.forEach { problem ->
+            if (problem.name.contains(command.toRegex())) {
+                val challenge = problem.newInstance() as Problem<*>
+                challenge.run()
+                found = true
+            }
+        }
+        if (!found) println("Unable to find a problem with that name! Try again")
     }
 }
---
+++ src/main/kotlin/me/circuitrcay/euler/challenges/Problem4.kt
@@ -0,0 +1,22 @@
+package me.circuitrcay.euler.challenges
+
+import me.circuitrcay.euler.Problem
+
+class Problem4 : Problem<String>() {
+    override fun calculate(): Any {
+        var largest = PalindromeResult(0, 0, 0)
+        for (i in 100..999) {
+            for (j in 100..999) {
+                if (isPal(i, j) && i * j > largest.product) largest = PalindromeResult(i, j)
+            }
+        }
+        return "${largest.first} * ${largest.second} = ${largest.product}"
+    }
+
+    private fun isPal(i: Int, j: Int): Boolean {
+        val str = (i * j).toString()
+        return str.substring(0, Math.floor(str.length / 2.0).toInt()) == str.substring(Math.ceil(str.length / 2.0).toInt(), str.length).reversed()
+    }
+
+}
+data class PalindromeResult(val first: Int, val second: Int, val product: Int = first * second)
