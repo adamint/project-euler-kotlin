d6dec0fd333337afba9c3f9a7bac36b482a78f18
CircuitRCAY <circuit@circuitrcay.me>
2018-03-10 11:37:43 +1100

    Problem 12 finished

--- src/main/kotlin/me/circuitrcay/euler/challenges/Problem12.kt
+++ src/main/kotlin/me/circuitrcay/euler/challenges/Problem12.kt
@@ -1,17 +1,23 @@
 package me.circuitrcay.euler.challenges
 
 import me.circuitrcay.euler.Problem
+import me.circuitrcay.euler.utils.generatePrimeFactorsWithFrequency
 
 class Problem12: Problem<String>() {
     override fun calculate(): Any {
-        return 0
+        return factorsOfNumber()
     }
-    fun getFactors(n: Int): Int {
-        return 0
-    }
-    fun gen(n: Int): Int {
-        var sum = 0
-        for (x in 1..n) sum += x
-        return sum
+
+    private fun factorsOfNumber() : Long {
+        var numFactors: Int
+        var counter = 2L
+        while (true) {
+            val triangleNumber = counter * (counter + 1) / 2
+            val temp = generatePrimeFactorsWithFrequency(triangleNumber)?.values?.map { it + 1 }
+            numFactors = temp!!.reduce { acc, i -> acc * i }
+            if (numFactors > 500) break
+            counter++
+        }
+        return (counter * (counter + 1) / 2)
     }
 }
---
+++ src/main/kotlin/me/circuitrcay/euler/utils/FrequencyMap.kt
@@ -0,0 +1,58 @@
+package me.circuitrcay.euler.utils
+
+/**
+ * Found this on Github oof https://github.com/tocttou/project-euler-kotlin/blob/master/main/src/utils/FrequencyMap.kt
+ */
+class FrequencyMap<K, Int>(private val b: MutableMap<K, kotlin.Int>)
+    : MutableMap<K, kotlin.Int> by b {
+    fun add(key: K, freq: kotlin.Int = 1) {
+        b.computeIfPresent(key) { _, b -> b + freq }
+        b.putIfAbsent(key, freq)
+    }
+
+    fun add(vararg pairs: Pair<K, kotlin.Int>) {
+        for (pair in pairs) {
+            val (key, freq) = pair
+            add(key, freq)
+        }
+    }
+
+    fun addAll(iter: Iterable<FrequencyMap<K, kotlin.Int>>) {
+        for (i in iter) {
+            for (j in i.keys) {
+                add(j, i.get(j)!!)
+            }
+        }
+    }
+
+    fun removeFreq(key: K, freq: kotlin.Int = 1) {
+        if (b.get(key) == null || freq < 1) return
+        else if (b.get(key)!! - freq < 1) b.remove(key)
+        b.computeIfPresent(key) { _, b -> b - freq }
+    }
+
+    override fun hashCode(): kotlin.Int {
+        return super.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (!(other is FrequencyMap<*, *>)) return false
+        return generateMutableMapFromFrequencyMap(this)
+                .equals(generateMutableMapFromFrequencyMap(other))
+    }
+
+    fun generateMutableMapFromFrequencyMap(obj: FrequencyMap<*, *>): MutableMap<Any?, Any?> {
+        val mMap = mutableMapOf<Any?, Any?>()
+        obj.forEach { t, u -> mMap.put(t, u) }
+        return mMap
+    }
+}
+
+fun <K, Int> frequencyMapOf(vararg pairs: Pair<K, kotlin.Int>): FrequencyMap<K, kotlin.Int> {
+    val mMap = mutableMapOf<K, kotlin.Int>()
+    val freqMap = FrequencyMap<K, kotlin.Int>(mMap)
+    for (pair in pairs) {
+        freqMap.add(pair)
+    }
+    return freqMap
+}
